639cee4335d2b2ac045c7fb4774055f6bd9b9b84
Navneet Kumar <navneetkumar.html@gmail.com>
2025-11-03 20:37:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Chai Code DevOps Assignment
+
+## Assignment Description
+
+The provided repository contains a basic full-stack application with the following components:
+
+1. A basic Express.js server with basic & courses endpoint
+2. A React.js frontend application
+3. Redis integration for API response caching
+4. MongoDB integration for data storage
+
+## Objective
+
+Your task is to containerize the application for a "Development" environment using Docker. Ensure that all components work seamlessly together within their respective containers and work as per the provided screenshots below.
+
+## Note
+
+The application environment variables are stored in a `.env` file. You can find the `.env.example` file in the respective directory. Copy it to `.env` and fill in the required values.
+
+## Screenshots
+
+### Home Page
+
+![Screenshot 1](./screenshots/One.png)
+
+### Load Data from MongoDB
+
+![Screenshot 2](./screenshots/Two.png)
+
+### API Response Caching
+
+![Screenshot 3](./screenshots/Three.png)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+// Assets
+import "./App.css";
+import chaiLogo from "./assets/chai.svg";
+
+// Components
+import CourseCard from "./components/CourseCard";
+
+const App = () => {
+  const [courses, setCourses] = useState([]);
+  const [dataSource, setDataSource] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchCourses = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/api/v1/courses`
+      );
+      const data = await response.json();
+      setCourses(data.data);
+      setDataSource(data.source);
+    } catch (error) {
+      console.error("Error fetching courses:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://chaicode.com" target="_blank">
+          <img src={chaiLogo} className="logo chai code" alt="Chai Code logo" />
+        </a>
+      </div>
+      <h1>ChaiCode DevOps</h1>
+      <div className="card">
+        <button onClick={fetchCourses} disabled={loading}>
+          {loading ? "Loading..." : "View courses"}
+        </button>
+      </div>
+      {dataSource && (
+        <p className="data-source">Data loaded from: {dataSource}</p>
+      )}
+      <div className="courses-grid">
+        {courses.map((course) => (
+          <CourseCard key={course.title} course={course} />
+        ))}
+      </div>
+      <p className="read-the-docs">Home for Programmers</p>
+    </>
+  );
+};
+
+export default App;
